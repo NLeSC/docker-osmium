312632a620c782df74bb67d91cf13ef0b06e2ae7
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-07-08 16:10:00 +0200

    Using automated build on dockerhub

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build and push Docker images to Dockerhub
-docker build -t nlesc/osmium latest &&
-docker build -t nlesc/osmium:develop develop &&
-docker push nlesc/osmium &&
-echo "Pushed docker repository nlesc/osmium"
