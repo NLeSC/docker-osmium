b1fc978573307d355f28dc713e96f737e0b5bd6f
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-07-09 13:28:01 +0200

    Able to run other commands in Docker

--- develop/start.sh
+++ develop/start.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
-exec /usr/bin/java -server -Xms10m -jar osmium.jar server
+#!/bin/bash
+if [ "$1" = 'osmium' ]; then
+  exec /usr/bin/java -server -Xms10m -jar osmium.jar server
+else
+  exec "$@"
+fi
