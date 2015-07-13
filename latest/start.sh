28afb960892b26f53fab64bf456015634fcc936a
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-07-13 10:00:26 +0200

    Add joblauncher.yml to command-line

--- latest/start.sh
+++ latest/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" = 'osmium' ]; then
-  exec /usr/bin/java -server -Xms10m -jar osmium.jar server
+  exec /usr/bin/java -server -Xms10m -jar osmium.jar server joblauncher.yml
 else
   exec "$@"
 fi
